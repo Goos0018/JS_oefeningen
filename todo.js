a027ae7e24190a1bc063bd6055e23295572e1ca6
Thomas Goos <goosthomas@gmail.com>
2020-09-16 15:55:11 +0200

    todolist

---
+++ todo.js
@@ -0,0 +1,9 @@
+// create an arrau with five todo's
+// you have x todo's
+// print the first and second to last items -> todo: walk the dog
+
+const todo = ['learn JS', 'eat', 'sleep', 'learn more JS', 'make something cool with JS']
+
+console.log(`You have ${todo.length} todo's`)
+console.log(`The first thing you have on your list is "${todo[0]}"`)
+console.log(`The second to last thing you have on your list is "${todo[todo.length -2]}"`)
