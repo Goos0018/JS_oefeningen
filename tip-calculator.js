dc66e78a397b10c7d31d8344cf7e629ef9bcc1a3
Thomas Goos <goosthomas@gmail.com>
2020-09-16 11:53:46 +0200

    tip-calculator with template string

--- tip-calculator.js
+++ tip-calculator.js
@@ -3,7 +3,8 @@
 let calculateTip = function(total = null, tipPercent = .2){
     let tip = total * tipPercent;
     let newTotal = total + tip;
-    return "the tip is $" + tip + ", your total is $" + newTotal;
+    //return "the tip is $ " + tip + ", your total is $ " + newTotal;
+    return `the tip is $${tip}, your total is now $${newTotal}`;
 }
 
 console.log(calculateTip(100,))
