f3a14348923321e932f455c841aa045a98ecfc4d
Thomas Goos <goosthomas@gmail.com>
2020-09-16 16:20:21 +0200

    manipulating todo list

--- todo.js
+++ todo.js
@@ -2,8 +2,17 @@
 // you have x todo's
 // print the first and second to last items -> todo: walk the dog
 
+// Delete the 3rd item
+// Add a new item onto the end
+// Remove the first item from the list
+
 const todo = ['learn JS', 'eat', 'sleep', 'learn more JS', 'make something cool with JS']
 
 console.log(`You have ${todo.length} todo's`)
 console.log(`The first thing you have on your list is "${todo[0]}"`)
 console.log(`The second to last thing you have on your list is "${todo[todo.length -2]}"`)
+
+console.log(todo.splice(2, 1))
+console.log(todo.push('also drink sometimes'))
+console.log(todo.shift())
+console.log(todo)
