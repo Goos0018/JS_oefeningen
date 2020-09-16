ce7caf4c217a5116125a766d99151219ce70b78c
Thomas Goos <goosthomas@gmail.com>
2020-09-16 11:35:40 +0200

    tip-calculator

---
+++ tip-calculator.js
@@ -0,0 +1,12 @@
+// total, tipPercent .2
+
+let calculateTip = function(total = null, tipPercent = .2){
+    let tip = total * tipPercent;
+    let newTotal = total + tip;
+    return "the tip is $" + tip + ", your total is $" + newTotal;
+}
+
+console.log(calculateTip(100,))
+console.log(calculateTip(100,.5))
+console.log(calculateTip(undefined))
+console.log(calculateTip(250,.11))
